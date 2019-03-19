a539a46cc161811c5581b9cedfa282b980d73611
Benjamin Hurley <benfromtech@gmail.com>
2019-03-19 16:18:43 -0400

    [update] updated groovy w/ rollback script

--- jenkins-job.groovy
+++ jenkins-job.groovy
@@ -10,7 +10,16 @@ pipeline {
          sh ''' 
             echo Initiating Rollback
             echo
+            echo Pulling any changes from remote repo
+            git pull
             echo
+            echo Rolling back last commit:
+            git log -1
+            echo
+            git revert -m 1 HEAD
+            echo
+            echo Pushing changes to remote repo
+            git push
          '''
        }
      }
